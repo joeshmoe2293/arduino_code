c1deb4125dc7e7ac626f90665b20770e71e208b2
Paul Vohs <Paul Vohs>
2017-03-07 21:37:31 -0700

    Testing middle servo

--- simple_walk_code/simple_walk_code.ino
+++ simple_walk_code/simple_walk_code.ino
@@ -1,6 +1,6 @@
-#define middle A0
-#define left A1
-#define right A2
+#define middle 10
+#define left 11
+#define right 12
 #define trig 5
 #define echo 6
 
@@ -11,7 +11,9 @@ Servo leftServo;
 Servo rightServo;
 
 unsigned long echo_value=0;
-unsigned long distance=0;
+unsigned long distance=10;
+
+int positions[3];
 
 void setup() 
 {
@@ -24,7 +26,13 @@ void setup()
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
 
-  distance=ping();
+  middleServo.write(90);
+  leftServo.write(90);
+  rightServo.write(90);
+
+  positions[0]=middleServo.read();
+  positions[1]=leftServo.read();
+  positions[2]=rightServo.read();
 }
 
 void liftOtherSide()
@@ -34,7 +42,7 @@ void liftOtherSide()
     for (int i=middleServo.read(); i<105; i++)
     {
       middleServo.write(i);
-      delay(250);
+      delay(10);
     }
   }
   else if (middleServo.read()>90)
@@ -42,26 +50,26 @@ void liftOtherSide()
     for (int i=middleServo.read(); i>75; i--)
     {
       middleServo.write(i);
-      delay(250);
+      delay(10);
     }
   }
 }
 
 void moveLegsForwards(Servo &servo)
 {
-  for (int i=servo.read(); i<105; i++)
+  for (int i=servo.read(); i<125; i++)
   {
     servo.write(i);
-    //delay(10);
+    delay(10);
   }  
 }
 
 void moveLegsBackwards(Servo servo)
 {
-  for (int i=servo.read(); i>75; i--)
+  for (int i=servo.read(); i>55; i--)
   {
     servo.write(i);
-    //delay(10);
+    delay(10);
   }  
 }
 
@@ -81,17 +89,35 @@ unsigned long ping()
 void loop() 
 {
   // NOT SURE HOW WELL THIS IS GOING TO WORK...
-  while (distance>5)
-  {
-    liftOtherSide();
-    delay(25);
-    moveLegsForwards(leftServo);
-    moveLegsBackwards(rightServo);
-    delay(25);
-    liftOtherSide();
-    delay(25);
-    moveLegsForwards(rightServo);
-    moveLegsBackwards(leftServo);
-    delay(25);
+  /*liftOtherSide();
+  moveLegsForwards(leftServo);
+  moveLegsBackwards(rightServo);
+  delay(25);
+  liftOtherSide();
+  delay(25);
+  moveLegsForwards(rightServo);
+  moveLegsBackwards(leftServo);
+  delay(25);*/
+  for (int i=50; i<110; i++)
+  { 
+    middleServo.write(i);
+    delay(10);
+    
+    positions[0]=middleServo.read();
+    positions[1]=leftServo.read();
+    positions[2]=rightServo.read();
+  
+    distance=ping();
+  
+    delay(500);
+  
+    for (int i=0; i<3; i++)
+    {
+      Serial.println(positions[i]);
+    }
+    
+    Serial.println(distance);
+  
+    Serial.println("Next loop...");
   }
 }
