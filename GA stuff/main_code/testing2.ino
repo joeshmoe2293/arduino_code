15214b7d460d88541e208ca5e8e400488b3c54ec
Paul Vohs <Paul Vohs>
2017-03-28 14:40:27 -0700

    ALMOST THERE :3

--- GA stuff/main_code/testing2.ino
+++ GA stuff/main_code/testing2.ino
@@ -1,10 +1,12 @@
+#define defaultIn 12
+#define defaultOut 13
+
 /* Class structure based on code provided at:
 http://www.theprojectspot.com/tutorial-post/creating-a-genetic-algorithm-for-beginners/ 
-HUGE thanks to fenneking and artemis in the c-plusplus channel on the "Programming" Discord server
+HUGE thanks to fenneking, artemism, and TheBev123 in the c-plusplus channel on the "Programming" Discord server
 for all the debugging and help*/
 
 #include "Arduino.h"
-using namespace std;
 
 namespace util
 {
@@ -41,10 +43,11 @@ namespace util
   };
 }
 
+/* This is assuming that we're using ultrasonic sensors (might make this better/bigger in future) */
 class Individual
 { 
   private:
-    int fitness=0;
+    int fitness=0, pin1, pin2;
     static constexpr int defaultGeneLength=6;
     util::array<float, defaultGeneLength> genes;
     float generateRandomGene(byte index);
@@ -59,11 +62,14 @@ class Individual
       CONSTANT2,
       CONSTANT3
     };
-  
+
+    Individual():Individual(defaultOut, defaultIn){};
+    Individual(int pinOut, int pinIn);
     void generateIndividual();
     float& getGene(byte index);
     void setGene(byte index, float value);
     int getSize() {return defaultGeneLength;}
+    void getMeasured();
 };
 
 template <byte populationSize>
@@ -88,21 +94,22 @@ class Algorithm
 
     static Individual crossover(Individual &indiv1, Individual &indiv2);
     static void mutate(Individual &indiv);
-    static template <byte popSize> Individual tournamentSelection(Population<popSize> &pop);
+    template <byte popSize> static Individual tournamentSelection(Population<popSize> &pop);
 
   public:
-    static template <byte popSize> Population<popSize> evolvePopulation(Population<popSize> &pop);
+    template <byte popSize> static Population<popSize> evolvePopulation(Population<popSize> &pop);
 };
 
 class FitnessCalc
 {
-  private: float solution;
+  private: 
+    float solution;
 
   public:
     FitnessCalc(float startSolution=25):solution(startSolution){}
     void setSolution(float newSolution=25);
-    int getFitness(Individual &individual);
-    template <byte popSize> static int getFittest(Population<popSize> &pop);
+    float getFitness(Individual &individual);
+    template <byte popSize> static float getFittest(Population<popSize> &pop);
 };
 
 template <byte popSize>
@@ -131,6 +138,12 @@ void Population<popSize>::saveIndividual(byte index, Individual &indiv)
   individuals[index]=indiv;
 }
 
+Individual::Individual(int pinOut, int pinIn):pin1(pinOut), pin2(pinIn)
+{
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, INPUT);
+}
+
 void Individual::generateIndividual()
 {
   for (byte i=0; i<genes.getSize(); i++)
@@ -147,10 +160,13 @@ float Individual::generateRandomGene(byte index)
 	switch (index)
   {
     case static_cast<byte>(OP1): case static_cast<byte>(OP2):
-      gene=static_cast<byte> (random(4));
+      gene=static_cast<int>(random(4));
       break;
     case static_cast<byte>(MEASURED):
       return genes[index]; // We don't want to generate a random value instead of using the measured one
+    case static_cast<byte>(CONSTANT3):
+      gene=static_cast<int>(random(10)+1);
+      break;
     default:
       gene=static_cast<float> (random(sizeof(byte)));
   }
@@ -166,6 +182,17 @@ float& Individual::getGene(byte index)
 void Individual::setGene(byte index, float value)
 {
   genes[index]=value;
+}
+
+void Individual::getMeasured()
+{
+  digitalWrite(pin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin1, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pin1, LOW);
+
+  setGene(0, static_cast<float>(pulseIn(pin2, HIGH)));
 }
 
 Individual Algorithm::crossover(Individual &indiv1, Individual &indiv2)
@@ -219,7 +246,16 @@ void Algorithm::mutate(Individual &indiv)
 template <byte popSize>
 Individual Algorithm::tournamentSelection(Population<popSize> &pop)
 {
-  // ADD STUFF HERE 
+  randomSeed(random(1000));
+  Population<popSize> tournamentPop(false);
+  for (byte i=0; i<popSize; i++)
+  {
+    byte randID=static_cast<int>(random()*popSize);
+    tournamentPop.saveIndividual(i, pop.getIndividual(randID));
+  }
+
+  Individual fittest=tournamentPop.getFittest();
+  return fittest;
 }
 
 template <byte popSize>
@@ -257,16 +293,28 @@ void FitnessCalc::setSolution(float newSolution)
 	solution=newSolution;
 }
 
-int FitnessCalc::getFitness(Individual &indiv)
+float FitnessCalc::getFitness(Individual &indiv)
 {
-  // ADD STUFF HERE  
+  using namespace util;
+  float fitness=0, calculated=0;
+  auto OP1=static_cast<byte>(indiv.getGene(Individual::OP1));
+  auto OP2=static_cast<byte>(indiv.getGene(Individual::OP2));
+  auto func1=operations::performOp[OP1];
+  auto func2=operations::performOp[OP2];
+  for (byte i=0; i<indiv.getGene(static_cast<byte>(Individual::CONSTANT3)); i++)
+  {
+    indiv.getMeasured();
+    calculated+=func2(func1(indiv.getGene(Individual::MEASURED), indiv.getGene(Individual::CONSTANT1)), indiv.getGene(Individual::CONSTANT2));
+  }
+  calculated/=indiv.getGene(static_cast<byte>(Individual::CONSTANT3));
+
+  return abs(calculated-solution);
 }
 
 template <byte popSize>
-int FitnessCalc::getFittest(Population<popSize> &pop)
+float FitnessCalc::getFittest(Population<popSize> &pop)
 {
-  int maxFitness=0;
-  int currentFitness=0;
+  float maxFitness=0, currentFitness=0;
   for (int i=0; i<popSize; i++)
   {
     currentFitness=pop.getIndividual(i).getFitness();
